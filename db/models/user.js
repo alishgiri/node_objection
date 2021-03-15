963e71bcf8bbbf6041ad03a2466cd14219d2193e
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-03-15 15:27:06 +0545

    Add user relation with channel

--- db/models/user.js
+++ db/models/user.js
@@ -4,6 +4,21 @@ class User extends Model {
     static get tableName() {
         return 'user';
     }
+
+    static get relationMappings() {
+        const Channel = require("./channel");
+        return {
+            channel: {
+                modelClass: Channel,
+                relation: Model.HasOneRelation,
+                join: {
+                    to: 'channel.id',
+                    from: 'user.channelId',
+                }
+            }
+        };
+    }
+
 }
 
 module.exports = User;
