1a05ca13a3332170df854ce5c92414f9ce23d890
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-03-15 15:27:29 +0545

    Update query to get change details

--- app.js
+++ app.js
@@ -11,13 +11,12 @@ app.use(express.json());
 app.get('/user/:id', async (req, res, next) => {
     const { id } = req.params;
     try {
-        const user = await User.query().findById(id);
+        const user = await User.query().findById(id).withGraphFetched("channel");
         res.send(user);
-
     } catch (e) {
         res.status(500).send(e)
     }
-})
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
